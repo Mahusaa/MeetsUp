98551911f86a9db1fd336d48b3fda757f36313c5
usamah <hakaro375@gmail.com>
2023-08-13 10:22:53 +0700

    fix image

--- src/app/details/[meetupid]/page.tsx
+++ src/app/details/[meetupid]/page.tsx
@@ -1,12 +1,32 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { getData } from "@/service/meetupService";
+import Image from "next/image";
 
 const MeetupDetails = async ({ params }: { params: { meetupid: string } }) => {
     const data = await getData();
-    
+    const dataMeet = data.find((meet) => meet.id === params.meetupid);
     return (
-      <h1>{params.meetupid}</h1>
+        <Card className="mx-80 my-5 px-5">
+            <CardHeader>
+                <CardTitle>
+                    {dataMeet?.title}
+                </CardTitle>
+                <CardDescription>
+                    {dataMeet?.address}
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Image src={dataMeet?.image || '/default-image.jpg'} alt={dataMeet?.title || "Yuhu"}
+                    width={600} height={600}>
+                </Image>
+                <CardDescription className="my-5">
+                    {dataMeet?.description}
+                </CardDescription>
+            </CardContent>
+
+        </Card>
+
     );
-  };
-  
-  export default MeetupDetails;
-  
+};
+
+export default MeetupDetails;
